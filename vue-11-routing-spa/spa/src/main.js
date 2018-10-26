3de75dc5474b46e5a681a899f840446e1186d56f
Zarko Stanisic <zarko.stanisic@live.com>
2018-10-26 16:17:52 +0200

    section 11. 150.

---
+++ vue-11-routing-spa/spa/src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router';
+import App from './App.vue'
+
+import {routes} from './routes'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: routes,
+    mode: 'history'
+});
+
+Vue.filter('currency', function(value) {
+    let formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 0
+    });
+    
+    return formatter.format(value);
+});
+
+export const eventBus = new Vue();
+
+new Vue({
+    el: '#app',
+    render: h => h(App),
+    router: router
+})
